434622bdc12228a0f125b2743bbc42a6fcc6e5e5
Rem Jassai <60391009+remjassai@users.noreply.github.com>
2023-02-20 02:32:49 +0100

    ex_12 advanced changes

--- src/it/develhope/ex_12.java
+++ src/it/develhope/ex_12.java
@@ -1,7 +1,9 @@
 package it.develhope;
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 public class ex_12 {
     public static void main(String[] args) {
@@ -24,20 +26,38 @@ public class ex_12 {
         ArrayList<Integer> numbers = new ArrayList<>();
 
         //Write your code here
-        numbers.add(23);
+
+       /* numbers.add(23);
         numbers.add(9);
         numbers.add(79);
-        numbers.add(15);
+        numbers.add(15);    This was in the beginning
         numbers.add(31);
         numbers.add(19);
         numbers.add(49);
         numbers.add(3);
+*/
+        //Create a new object of the Random class
+        Random randNum = new Random();
+
+        //create a fori cycle that runs 8 times
+        for (int i = 0; i < 8; i++) {
+
+            // create a random number in the integer's range but < 23 and assign it to a var named next
+            int next = randNum.nextInt(24);
+
+            //add the random number to the list of numbers
+            numbers.add(next);
+        }
 
         System.out.println(numbers.get(0));
-
+/*
         numbers.remove(numbers.get(0));
+        numbers.remove(0);                  This was in the beginning
         numbers.remove(0);
-        numbers.remove(0);
+*/
+        //remove the first tree numbers from the list
+        numbers.subList(0,3).clear();
+
 
         System.out.println(numbers.get(0));
 /*
@@ -66,25 +86,37 @@ public class ex_12 {
         //Write your code here
 
 
-        ArrayList<String> menuItems = new ArrayList<>();
-        menuItems.add("cheeseburger");
-        menuItems.add("hamburger");
-        menuItems.add("fish-burger");
 
-        ArrayList<Double> menuPrice = new ArrayList<>();
-        menuPrice.add(4.55);
-        menuPrice.add(4.00);
-        menuPrice.add(7.00);
+        ArrayList<String> menuItems = new ArrayList<>(
+                Arrays.asList(
+                        "cheeseburger",
+                        "hamburger",
+                        "fish-burger"
+                ));
 
+        ArrayList<Double> menuPrices = new ArrayList<>(
+                Arrays.asList(
+                        4.55,
+                        4.00,
+                        7.00
+                ));
+//        ArrayList<String> menuItems = new ArrayList<>();
+//        menuItems.add("cheeseburger");
+//        menuItems.add("hamburger");           This was in the beginning
+//        menuItems.add("fish-burger");
 
+//        ArrayList<Double> menuPrice = new ArrayList<>();
+//        menuPrice.add(4.55);
+//        menuPrice.add(4.00);           This was in the beginning
+//        menuPrice.add(7.00);
 
-        System.out.println("Item: "+ menuItems.get(0) + "\n Cost: " + menuPrice.get(0));
-        System.out.println("Item: "+ menuItems.get(1) + "\n Cost: " + menuPrice.get(1));
-        System.out.println("Item: "+ menuItems.get(2) + "\n Cost: " + menuPrice.get(2));
+        for (int i = 0; i < menuPrices.size(); i++) {
+            System.out.println("Item: "+ menuItems.get(i) + "\n Cost: " + menuPrices.get(i));
+        }
 
-
-
-
+//        System.out.println("Item: "+ menuItems.get(0) + "\n Cost: " + menuPrices.get(0));
+//        System.out.println("Item: "+ menuItems.get(1) + "\n Cost: " + menuPrices.get(1));            This was in the beginning
+//        System.out.println("Item: "+ menuItems.get(2) + "\n Cost: " + menuPrices.get(2));
 
     }
 
