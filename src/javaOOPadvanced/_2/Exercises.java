a5312b3d2d5b66f565d9d8d0ec3a3a24c4816fcb
Rem Jassai <60391009+remjassai@users.noreply.github.com>
2023-03-03 23:40:13 +0100

    Maps_ex

--- src/javaOOPadvanced/_2/Exercises.java
+++ src/javaOOPadvanced/_2/Exercises.java
@@ -15,13 +15,19 @@ public class Exercises {
      */
     private static void exercise1() {
         // 1a. Create a map of integers to strings
-        Map<Integer, String> myFirstMap = new HashMap<>();
-
         // 1b. Add 3 elements to the map .put
         // 1c. Print the map
         // 1d. Access an element in the map with .get
         // 1e. Remove an element from the map .remove
         // 1f. Print the map again to confirm the element was removed
+        HashMap<Integer, String> mySecondMap = new HashMap<>();
+        mySecondMap.put( 1, "One");
+        mySecondMap.put( 2, "Two");
+        mySecondMap.put( 3, "Three");
+        System.out.println(mySecondMap);
+        System.out.println(mySecondMap.get(1));
+        mySecondMap.remove(2);
+        System.out.println(mySecondMap);
     }
 
     /**
@@ -35,13 +41,20 @@ public class Exercises {
         menuCostsInEuro.put("Burger", 8.50);
 
         String mostExpensiveMenuItemName = null;
-
+        double cost = 0;
         // 2a. Loop over the map entries with menuCostsInEuro.entrySet()
         //     and set out the most expensive items name
         //     use for(Map.Entry<String, Double> entry : menuCostsInEuro.entrySet()) and entry.getKey and entry.getValue
         //     to do so
 
         // Your code
+        for (Map.Entry<String, Double> entry : menuCostsInEuro.entrySet()) {
+            if (entry.getValue() > cost) {
+                cost = entry.getValue();
+                mostExpensiveMenuItemName = entry.getKey();
+            }
+        }
+
 
         if (mostExpensiveMenuItemName.equals("Pizza")) {
             System.out.println("2a. Correct!");
@@ -69,6 +82,11 @@ public class Exercises {
         // 3a. Use a for loop with fruitWeights.values() and sum up the total weight
 
         // Your code
+
+        for(Integer entry : fruitWeights.values()) {
+            totalWeight += entry;
+        }
+
 
         if (totalWeight == fruitWeights.values().stream().mapToInt(Integer::intValue).sum()) {
             System.out.println("3. Correct!");
